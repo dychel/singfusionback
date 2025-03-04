c83f5e65751a6e52502b8ba2b3e79354c26e5927
dychel <dycheloko20@gmail.com>
2025-03-04 18:11:29 +0100

    first commit

---
+++ src/main/java/com/singfusion/singfusion/SingfusionApplication.java
@@ -0,0 +1,13 @@
+package com.singfusion.singfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingfusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singfusion/singfusion/SingfusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singfusion.singfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingfusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
